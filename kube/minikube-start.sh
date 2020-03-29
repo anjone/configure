0cc37a7e00537fafdf054671a10ed77d757e2ed8
joe <anjone@126.com>
2020-03-29 23:29:22 +0800

    add driver=none start command

--- kube/minikube-start.sh
+++ kube/minikube-start.sh
@@ -1,2 +1,3 @@
 minikube start --logtostderr --v=3 --image-mirror-country="cn" --registry-mirror="https://dockerhub.azk8s.cn"
 
+minikube start --driver=none --image-mirror-country='cn' --registry-mirror='https://dockerhub.azk8s.cn' --image-repository='registry.cn-hangzhou.aliyuncs.com/google_containers' --alsologtostderr -v=1
